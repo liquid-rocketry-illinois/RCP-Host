e5edc9609dd114cc7ea407f6c67563e81dfa5ee7
Jacob Baumel <jacob.a.baumel@gmail.com>
2025-08-09 22:08:52 -0500

    Actually fixed name collision with VERSION.cpp

--- cmake/gen_version.cmake
+++ cmake/gen_version.cmake
@@ -10,4 +10,4 @@ string(APPEND CODESTR "\"\;\nconst char* const RCP_VERSION_END = RCP_VERSION + "
 string(LENGTH ${vstr} vstrlen)
 string(APPEND CODESTR ${vstrlen})
 string(APPEND CODESTR "\;")
-file(WRITE ${BIN}/RCPVERSION.cpp ${CODESTR})
+file(WRITE ${BIN}/VERSION.cpp ${CODESTR})
